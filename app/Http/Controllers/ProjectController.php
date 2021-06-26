71b7392b093f1c24e52813b202889b9bed09b09e
Jailton Abilio de Oliveira Neto <jailton.neto@lessclick.com.br>
2021-06-26 11:17:14 -0300

    feature: added project controller with store and index functions

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+
+    public function index(Request $request)
+    {
+        $projects = Project::all();
+
+        return view('projects.index', compact('projects'));
+    }
+    
+    public function store(Request $request)
+    {
+     
+        $parameters = $request->all();
+        $created = Project::create($parameters);
+
+    }
+}
